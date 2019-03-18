dcdf12e17ebb2380906f1740c0a094163c7f8ad6
Nex <nex@nex.sx>
2019-03-18 17:18:07 +0100

    Small change

--- api_events.go
+++ api_events.go
@@ -24,14 +24,14 @@ import (
 	"github.com/nu7hatch/gouuid"
 )
 
-type RequestFetchEvent struct {
+type RequestFetchEvents struct {
 	Key string `json:"key"`
 }
 
 func apiEventsFetch(w http.ResponseWriter, r *http.Request) {
 	// We decode the request to an Event.
 	decoder := json.NewDecoder(r.Body)
-	var req RequestFetchEvent
+	var req RequestFetchEvents
 	err := decoder.Decode(&req)
 	if err != nil {
 		errorWithJSON(w, "Unable to parse event", http.StatusBadRequest, err)
